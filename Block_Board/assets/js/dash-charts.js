8b460e040511b69e7d24414d302e5375f8732e8e
Jason <jasonyl13579@gmail.com>
2022-01-19 01:09:46 -0500

    block board http-> https

--- Block_Board/assets/js/dash-charts.js
+++ Block_Board/assets/js/dash-charts.js
@@ -56,7 +56,6 @@
 		sentiment = sdata[0];
 		
 		var compound = parseInt((sentiment.compound * 100).toFixed(1));
-		console.log(compound)
 		$(twitter_tag).text(compound + '%');
 		$(document).ready(function() {
 			info = new Highcharts.Chart({
@@ -101,12 +100,11 @@
 
 /*** First Chart in Dashboard page ***/
 
-	console.log('hi sebtunebt', sentiment.volume)
 	var maxvolume = d3.max(sdata, function(d){return d.volume})
 	var minvolume = d3.min(sdata, function(d){return d.volume})
 	var vol_percent = parseInt(((sentiment.volume - minvolume)/(maxvolume - minvolume) * 100)).toFixed(1);
 	vol_percent = +vol_percent
-	console.log('max value', maxvolume, 'min value', minvolume, 'percentvol', vol_percent)
+	//console.log('max value', maxvolume, 'min value', minvolume, 'percentvol', vol_percent)
 
 	$(volume_tag).text(vol_percent + '%');
 
